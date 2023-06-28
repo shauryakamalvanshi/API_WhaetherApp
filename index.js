fbe636e6f38a59e272c02b72fcd4bdf7ed09a840
Shaurya Kamalvanshi <122744383+shauryakamalvanshi@users.noreply.github.com>
2023-06-28 19:36:01 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,42 @@ const http = require('http');
 const fs = require("fs");
 const requests = require("requests");
 
+const replaceVal = (tempVal, orgVal) => {
+  const trep = orgVal.main.temp;
+  const C = (trep - 273.15).toFixed(2);
+  
+  const Re = orgVal.main.feels_like;
+  const R = (Re - 273.15).toFixed(2);
+
+  let temperature = tempVal.replace("{%tempval%}", C);
+  temperature = temperature.replace("{%loaction%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%humidity%}", orgVal.main.humidity);
+  temperature = temperature.replace("{%windSpeed%}", orgVal.wind.speed);
+  temperature = temperature.replace("{%pressure%}", orgVal.main.pressure);
+  temperature = temperature.replace("{%feelLike%}", R);
+  return temperature;
+};
+
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
-const replaceVal = (tempVal, orgVal) => {
-    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
-    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
-    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
-    temperature = temperature.replace("{%location%}", orgVal.name);
-    temperature = temperature.replace("{%country%}", orgVal.sys.country);
-    return temperature;
-  };
-  
-
-
-  const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-      requests('https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=59c6f8f6316df5a0ec6366dff1099cd6')
-        .on('data', (chunk) => {
-          const objdata = JSON.parse(chunk);
-          const arrData = [objdata];
-          const realTimeData = arrData.map((val) => {
-            return replaceVal(homeFile, val);
-          });
-          res.write(realTimeData[0]);
-          res.end();
-        })
-        .on('end', (err) => {
-          if (err) return console.log('connection closed due to errors', err);
-        });
-    }
-  });
-  
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests('https://api.openweathermap.org/data/2.5/weather?q=Noida&appid=59c6f8f6316df5a0ec6366dff1099cd6')
+      .on('data', (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+        res.write(realTimeData);
+      })
+      .on('end', (err) => {
+        if (err) {
+          console.log('connection closed due to errors', err);
+        }
+        // Move res.end() here, outside the 'on('end')' event handler
+        res.end();
+      });
+  }
+});
 
 server.listen(8000, "127.0.0.1");
